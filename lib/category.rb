4aa7d27a819cc6b449b47a2ceb44973603fb72e7
Peggy Condon <peggyc3@gmail.com>
2014-08-13 14:21:36 -0700

    starts with empty array

--- lib/category.rb
+++ lib/category.rb
@@ -1,10 +1,22 @@
 class Category
-    attr_reader  :id
-    attr_accessor :name, :budget
+  attr_reader  :id
+  attr_accessor :name, :budget
 
-    def initialize(attributes)
-        @name = attributes['name']
-        @budget = attributes['budget']
-        @id = attributes['id'].to_i
-    end
+  def initialize(attributes)
+      @name = attributes['name']
+      @budget = attributes['budget']
+      @id = attributes['id'].to_i
+  end
+
+  def self.all
+
+    categories = []
+
+  end
+
+  def save
+  result = DB.exec("INSERT INTO category (name, budget) VALUES (#{@name},'#{@budget}') RETURNING id;")
+  @id = result.first['id'].to_i
+  end
+
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 describe 'Category' do
 
-it 'initializes a category with a name and a budget amount' do
-    test_category = Category.new({'name' => 'office supplies', 'budget' => 1000.00, 'id' => 7})
-    expect(test_category).to be_an_instance_of Category
+  it 'initializes a category with a name and a budget amount' do
+      test_category = Category.new({'name' => 'office supplies', 'budget' => 1000.00, 'id' => 7})
+      expect(test_category).to be_an_instance_of Category
   end
+
+
+describe '.all' do
+  it 'should be empty at first' do
+    test_expense = Expense.new({ 'amount' => 249.00, 'description' => 'office chair', 'date' => '2005-12-10', 'company_id' => 2})
+    expect(Expense.all).to eq []
+  end
+end
+
+
 end
