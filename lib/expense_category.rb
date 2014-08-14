81dd646acb0c408ee96ac190b759988ddfd93691
Peggy Condon <peggyc3@gmail.com>
2014-08-13 18:19:37 -0700

    saves the expense_category to the database

--- lib/expense_category.rb
+++ lib/expense_category.rb
@@ -3,16 +3,28 @@ class Expense_category
   attr_accessor :category_id, :expense_id
 
   def initialize(attributes)
-      @category_id = attributes['category_id']
-      @expense_id = attributes['expense_id']
+      @category_id = attributes['category_id'].to_i
+      @expense_id = attributes['expense_id'].to_i
       @id = attributes['id'].to_i
   end
 
   def self.all
 
     expense_categories = []
-
+    results = DB.exec("SELECT * FROM expense_category")
+    results.each do |result|
+      expense_categories << Expense_category.new(result)
+    end
+    expense_categories
   end
 
+  def save
+   result = DB.exec("INSERT INTO expense_category (expense_id, category_id) VALUES (#{@expense_id}, #{@category_id}) RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+
+  def ==(another)
+    self.expense_id == another.expense_id && self.category_id == another.category_id
+  end
 
 end
--- spec/expense_category_spec.rb
+++ spec/expense_category_spec.rb
@@ -3,17 +3,25 @@ require 'spec_helper'
 describe 'Expense_category' do
 
   it 'initializes an expense_category with an expense_id and a category_id' do
-    test_expense = Expense.new({ 'amount' => 249.00, 'description' => 'office chair', 'date' => '2005-12-10', 'company_id' => 2, 'id' => 3})
+    test_expense = Expense.new({ 'amount' => 249.00, 'description' => 'office chair', 'date' => '2005-12-10','company_id' => 3})
     test_category = Category.new({ 'name' => 'furniture', 'budget' => 10000, 'id' => 4})
     test_category.save
-    test_expense_category = Expense_category.new({ 'expense_id' => 3, 'category_id' => 4})
+    test_expense_category = Expense_category.new({'expense_id' => 3, 'category_id' => 4, })
     expect(test_expense_category).to be_an_instance_of Expense_category
   end
 
   describe '.all' do
     it 'should be empty at first' do
-      test_expense_category = Expense_category.new({ 'expense_id' => 3, 'category_id' => 4})
+      test_expense_category = Expense_category.new({'expense_id' => 3, 'category_id' => 4})
       expect(Expense_category.all).to eq []
+    end
+  end
+
+  describe '#save' do
+    it 'saves the expense_category to the database' do
+    test_expense_category1 = Expense_category.new({'expense_id' => 5, 'category_id' => 6})
+    test_expense_category1.save
+    expect(Expense_category.all).to eq [test_expense_category1]
     end
   end
 end
